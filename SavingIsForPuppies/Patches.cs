ef5a42016a33c9651f49688cdad2c009faff787e
FlukeyFiddler <robertovdmeer@gmail.com>
2018-05-29 14:13:52 +0200

    Initial setup

--- SavingIsForPuppies/Patches.cs
+++ SavingIsForPuppies/Patches.cs
@@ -1,13 +1,15 @@
 ﻿using BattleTech.UI;
 using Harmony;
+using nl.flukeyfiddler.Utils;
 
-namespace nl.flukeyfiddler.SavingIsForPuppies
+namespace nl.flukeyfiddler.SavIsForPuppies
 {
     [HarmonyPatch(typeof(MainMenu), "HandleEscapeKeypress")]
     public class MainMenu_HandleEscapeKeyPress_Patch
     {
         public static void Postfix(MainMenu __instance)
         {
+            Logger.LogLine("It works");
         }
     }
 }
