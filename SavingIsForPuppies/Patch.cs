aecf3013a1072dacb2e79de57e96697d2c34fbe4
FlukeyFiddler <robertovdmeer@gmail.com>
2018-05-29 13:47:06 +0200

    Initial commit

---
+++ SavingIsForPuppies/Patch.cs
@@ -0,0 +1,13 @@
+﻿using BattleTech.UI;
+using Harmony;
+
+namespace nl.flukeyfiddler.SavingIsForPuppies
+{
+    [HarmonyPatch(typeof(MainMenu), "HandleEscapeKeypress")]
+    public class MainMenu_HandleEscapeKeyPress_Patch
+    {
+        public static void Postfix(MainMenu __instance)
+        {
+        }
+    }
+}
---
+++ SavingIsForPuppies/SavingIsForPuppies.cs
@@ -0,0 +1,14 @@
+﻿using Harmony;
+using System.Reflection;
+
+namespace nl.flukeyfiddler.SavingIsForPuppies
+{
+    public class SavingIsForPuppies
+    {
+        public static void Init()
+        {
+            var harmony = HarmonyInstance.Create("nl.flukeyfiddler.SavingIsForPuppies");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
